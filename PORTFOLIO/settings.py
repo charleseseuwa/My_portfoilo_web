795ec7d8f438bfdc5de49a96a986fa0d3af10bc3
Charles <charlieu2000@gmail.com>
2023-07-15 06:42:33 +0100

    'message'

--- PORTFOLIO/settings.py
+++ PORTFOLIO/settings.py
@@ -133,7 +133,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
+EMAIL_HOST_USER = 'ch@gmail.com'
+EMAIL_HOST_PASSWORD = 'password'
+
 EMAIL_USE_TLS = True
--- web/views.py
+++ web/views.py
@@ -5,49 +5,69 @@ from django.http import HttpResponse
 def index_page(request):
     
     
-    if request.method == 'GET':
-        return render(request, 'web/index.html')
     
-    elif request.method == 'POST':
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        form_data = {
+            'name':name,
+            'email':email,
+            'phone':phone,
+            'message':message,
+        }
+        message = '''
+        From:\n\t\t{}\n
+        Message:\n\t\t{}\n
+        Email:\n\t\t{}\n
+        Phone:\n\t\t{}\n
+        '''.format(form_data['name'], form_data['message'], form_data['email'],form_data['phone'])
+        send_mail('You got a mail!', message, '', ['ch@gmail.com']) #  enter your email address
         
-        message_name = request.POST.get("name")
-        message_email = request.POST.get("email")
-        message_subject = request.POST.get("subject")
-        message = request.POST.get("message")
+    return render(request, 'web/index.html', {})
 
-        
-        send_mail(
-            
-            message_name,
-            message,
-            message_email,
-            ['charlesuwagboe1984@gmail.com']
-     
-     
-            
 
-        )
-       
-        return HttpResponse("Your message has been sent")
     # if request.method == 'POST':
+        
     #     name = request.POST.get('name')
     #     email = request.POST.get('email')
     #     subject = request.POST.get('subject')
     #     message = request.POST.get('message')
-    #     form_data = {
-    #         'name':name,
-    #         'email':email,
-    #         'subject':subject,
-    #         'message':message,
-    #     }
-    #     message = '''
-    #     From:\n\t\t{}\n
-    #     Message:\n\t\t{}\n
-    #     Email:\n\t\t{}\n
-    #     subject:\n\t\t{}\n
-    #     '''.format(form_data['name'], form_data['email'],form_data['subject'],form_data['message'])
-    #     print('You got a mail!', message, '', ['charlesuwagboe1984@gmail.com']) # TODO: enter your email address
+    #     send_mail(subject, message, email, ['charlesuwagboe1984@gmail.com'], fail_silently=False)
+        
+    #     form_data = {name:'name', email:"email", subject:'subject', message:'message'}
+        
         
     # return render(request, 'web/index.html', {})
    
+    
+    # if request.method == 'GET':
+    #     return render(request, 'web/index.html')
+    
+    # elif request.method == 'POST':
+        
+    #     message_name = request.POST.get("name")
+    #     message_email = request.POST.get("email")
+    #     message_subject = request.POST.get("subject")
+    #     message = request.POST.get("message")
+
+        
+    #     send_mail(
+            
+    #         message_name,
+    #         message_subject,
+    #         message,
+    #         message_email,
+    #         ['cha@gmail.com']
+     
+     
+            
+
+    #     )
+       
+    #     return HttpResponse("Your message has been sent")
+   
 # Create your views here.
+
+    
